bcfe7ce69b52f0877d25e75194f84a0f6ee244d8
MartinBand <martinbandoni@hotmail.com>
2025-02-24 00:14:03 -0300

    Segundo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import './App.css';
 
 const App = () =>{
 
-  let nombre = `juan carlos`
+  // let nombre = `juan carlos`
+  let datos = {
+    nombre:'Saul',
+    apellido:'Gonzalez',
+    edad:47
+  }
+
+  let ciudad ={
+    nombre:'Mexico',
+    habitantes: 60000000
+  }
+
+  let frutas = ['manzana','pera','uva']
+
+  //destructuring
+
+  const {nombre} = datos
+  const {nombre:nombreCiudad} = ciudad
 
   return (
     <div>
-      <h1 className='cambiarColor'>{nombre}</h1>
+      <h1 className='cambiarColor'>{frutas[2]} {nombreCiudad}</h1>
     </div>
 
   );
