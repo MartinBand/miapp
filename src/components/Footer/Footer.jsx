9b7e2225e7fe04986d7b64589cd99979cebfe496
MartinBand <martinbandoni@hotmail.com>
2025-02-24 00:17:14 -0300

    Tercero

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+return(
+<div className='pie'>Footer</div>
+)
+}
+
+export {Footer}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return(
+        <header className="miClase">
+            <h2>logo</h2>
+            <div>
+                <a href="#">Inicio</a>
+                <a href="#">Info</a>
+                <a href="#">Shop</a>
+                <a href="#">Contacto</a>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
