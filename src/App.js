f5a8c3dff7db069f6d1ab9143d39287a38eb057d
MartinBand <martinbandoni@hotmail.com>
2025-02-26 11:30:09 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -21,9 +21,14 @@ const App = () =>{
   const {nombre} = datos
   const {nombre:nombreCiudad} = ciudad
 
+  const haceClick = () => {
+    console.log (`hiciste click`)
+  }
+
   return (
     <div>
       <h1 className='cambiarColor'>{frutas[2]} {nombreCiudad}</h1>
+      <button onClick={haceClick}>Hace click</button>
     </div>
 
   );
